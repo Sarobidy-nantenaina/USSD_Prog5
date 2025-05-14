76460a4b2e9f2092adf9a3f86fa58d6ac11d4cbc
Sarobidy-nantenaina <sarobidyrakotondrazafy1@gmail.com>
2025-05-14 18:53:57 +0300

    add configuration for linter

---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: globals.node,
+    },
+    plugins: { js },
+    rules: {
+      semi: ["error", "always"],
+      quotes: ["error", "single"],
+    },
+    extends: ["js/recommended"],
+  },
+  {
+    files: ["**/*.ts"],
+    ...tseslint.configs.recommended[0],
+  },
+]);
